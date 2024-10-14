574823ee9b4a9f86b1050aee97988f05ab253781
andrepettersson <andredavidpettersson@gmail.com>
2024-10-14 09:56:36 +0200

    fixad

--- obj/Debug/net8.0/Uppgift1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Uppgift1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uppgift1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e573e40d694e376dc7d315036052b7062427eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7504da82c40db7ff1b1bb58a1109169c48e6716")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uppgift1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uppgift1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
